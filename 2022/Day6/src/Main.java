ffda2f8abd93804da0b7f42c9c7323b9e5fa4059
nikolakolev1 <nikola.kolev1213@gmail.com>
2022-12-07 15:47:30 +0000

    Minor changes to Day6

--- 2022/Day6/src/Main.java
+++ 2022/Day6/src/Main.java
@@ -1,59 +1,61 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.Array;
-import java.util.*;
-//import java.util.ArrayList;
-
+import java.util.Scanner;
 
 public class Main {
+    // Sorry for how unreadable that is
     public static void main(String[] args) {
-        mainMethod("part1");
+        mainMethod("part1"); // "part1" || whatever string
     }
 
-    public static void mainMethod(String part) {
+    public static void mainMethod(String partStr) {
         try {
             File input = new File("input.txt");
             Scanner myScanner = new Scanner(input);
 
-//            ArrayList<Character> characters= new ArrayList<>();
-            Character[] currentQueue = new Character[14];
+            int partInt = partStr.equals("part1") ? 4 : 14;
+            Character[] currentChars = new Character[partInt];
             if (myScanner.hasNext()) {
                 String test = myScanner.next();
-//                System.out.println(test);
-//                System.out.println("!!!!!!!!!");
                 String[] characters = test.split("");
-                for (int i = 0; i < 14; i++) {
-                    currentQueue[i] = (characters[i].charAt(0));
+                for (int i = 0; i < partInt; i++) {
+                    currentChars[i] = (characters[i].charAt(0));
                 }
 
-
                 // check for repetition
-                for (int i = 14; i < characters.length; i++) {
-                    int something = 0;
-                    for (int j = 0; j < 14; j++) {
-                        char temp = currentQueue[j];
-                        for (int k = j + 1; k < 14; k++) {
-                            if (currentQueue[k] != temp) {
-                                something++;
+                for (int i = partInt; i < characters.length; i++) {
+                    int counter = 0;
+                    for (int j = 0; j < partInt; j++) {
+                        char temp = currentChars[j];
+                        for (int k = j + 1; k < partInt; k++) {
+                            if (currentChars[k] != temp) {
+                                counter++;
                             }
                         }
                     }
 
-                    if (something == 91) {
+                    if (counter == sumOfSmallerInts(partInt)) {
                         System.out.println(i);
                         return;
                     }
 
-                    int[] copy = new int[13];
-                    for (int j = 1; j < 14; j++) {
-                        currentQueue[j-1] = currentQueue[j];
+                    for (int j = 1; j < partInt; j++) {
+                        currentChars[j - 1] = currentChars[j];
                     }
-                    currentQueue[13] = (characters[i].charAt(0));
+                    currentChars[partInt - 1] = (characters[i].charAt(0));
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static int sumOfSmallerInts(int bigInt) {
+        int sum = 0;
+        for (int i = 1; i < bigInt; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
