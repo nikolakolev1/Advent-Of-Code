66b55353c577919cf5c86c1f8cd70e15635be456
nikolakolev1 <nikola.kolev1213@gmail.com>
2022-12-09 16:47:42 +0000

    Day10 Prep

---
+++ 2022/Day10/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void mainMethod() {
+        try {
+            File input = new File("input.txt");
+            Scanner myScanner = new Scanner(input);
+
+            while (myScanner.hasNextLine()) {
+                String thisRow = myScanner.nextLine();
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
