846daa9d37a6a75cfca5951af7258b0ba88450de
nikolakolev1 <nikola.kolev1213@gmail.com>
2022-12-10 10:22:57 +0000

    Day8

--- 2022/Day8/src/Main.java
+++ 2022/Day8/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         mainMethod();
     }
 
-    public static void mainMethod() {
+    private static void mainMethod() {
         loadData();
         provideNeighbours();
 
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("Part 2: " + answerP2);
     }
 
-    public static void loadData() {
+    private static void loadData() {
         try {
             File input = new File("input.txt");
             Scanner myScanner = new Scanner(input);
@@ -50,7 +50,7 @@ public class Main {
         }
     }
 
-    public static void provideNeighbours() {
+    private static void provideNeighbours() {
         int rowSize = forest.get(0).size();
 
         for (int i = 0; i < forest.size(); i++) {
@@ -74,5 +74,167 @@ public class Main {
                 forest.get(i).get(j).assignNeighbours(leftNeigh, rightNeigh, topNeigh, bottomNeigh);
             }
         }
+    }
+}
+
+class Tree {
+    private final int height;
+    private boolean visFrLeft, visFrRight, visFrTop, visFrBottom;
+    public Tree leftNeighbour, rightNeighbour, topNeighbour, bottomNeighbour;
+
+    public Tree(int height) {
+        this.height = height;
+    }
+
+    public void assignNeighbours(Tree leftN, Tree rightN, Tree topN, Tree bottomN) {
+        leftNeighbour = leftN;
+        rightNeighbour = rightN;
+        topNeighbour = topN;
+        bottomNeighbour = bottomN;
+    }
+
+    private int maxHeightToL = 0;
+
+    private boolean checkVisLeft() {
+        Tree leftN = leftNeighbour;
+        while (leftN != null) {
+            if (leftN.height >= maxHeightToL) {
+                maxHeightToL = leftN.height;
+                if (maxHeightToL >= height) {
+                    return false;
+                }
+            }
+            leftN = leftN.leftNeighbour;
+        }
+        return true;
+    }
+
+    private int maxHeightToR;
+
+    private boolean checkVisRight() {
+        Tree rightN = rightNeighbour;
+        while (rightN != null) {
+            if (rightN.height >= maxHeightToR) {
+                maxHeightToR = rightN.height;
+                if (maxHeightToR >= height) {
+                    return false;
+                }
+            }
+            rightN = rightN.rightNeighbour;
+        }
+        return true;
+    }
+
+    private int maxHeightToT;
+
+    private boolean checkVisTop() {
+        Tree topN = topNeighbour;
+        while (topN != null) {
+            if (topN.height >= maxHeightToT) {
+                maxHeightToT = topN.height;
+                if (maxHeightToT >= height) {
+                    return false;
+                }
+            }
+            topN = topN.topNeighbour;
+        }
+        return true;
+    }
+
+    private int maxHeightToB;
+
+    private boolean checkVisBottom() {
+        Tree bottomN = bottomNeighbour;
+        while (bottomN != null) {
+            if (bottomN.height >= maxHeightToB) {
+                maxHeightToB = bottomN.height;
+                if (maxHeightToB >= height) {
+                    return false;
+                }
+            }
+            bottomN = bottomN.bottomNeighbour;
+        }
+        return true;
+    }
+
+    public void checkVisibilities() {
+        visFrLeft = false;
+        visFrRight = false;
+        visFrTop = false;
+        visFrBottom = false;
+        if (checkVisLeft()) {
+            visFrLeft = true;
+        } else if (checkVisRight()) {
+            visFrRight = true;
+        } else if (checkVisTop()) {
+            visFrTop = true;
+        } else if (checkVisBottom()) {
+            visFrBottom = true;
+        }
+    }
+
+    private int scenicToL = 0;
+
+    private void scenicLeft() {
+        Tree leftN = leftNeighbour;
+        while (leftN != null) {
+            scenicToL++;
+            if (leftN.height >= height) {
+                return;
+            }
+            leftN = leftN.leftNeighbour;
+        }
+    }
+
+    private int scenicToR = 0;
+
+    private void scenicRight() {
+        Tree rightN = rightNeighbour;
+        while (rightN != null) {
+            scenicToR++;
+            if (rightN.height >= height) {
+                return;
+            }
+            rightN = rightN.rightNeighbour;
+        }
+    }
+
+    private int scenicToT = 0;
+
+    private void scenicTop() {
+        Tree topN = topNeighbour;
+        while (topN != null) {
+            scenicToT++;
+            if (topN.height >= height) {
+                return;
+            }
+            topN = topN.topNeighbour;
+        }
+    }
+
+    private int scenicToB = 0;
+
+    private void scenicBottom() {
+        Tree bottomN = bottomNeighbour;
+        while (bottomN != null) {
+            scenicToB++;
+            if (bottomN.height >= height) {
+                return;
+            }
+            bottomN = bottomN.bottomNeighbour;
+        }
+    }
+
+    public int scenicScore() {
+        scenicLeft();
+        scenicRight();
+        scenicTop();
+        scenicBottom();
+        return scenicToL * scenicToR * scenicToT * scenicToB;
+    }
+
+    public boolean isVisible() {
+        checkVisibilities();
+        return (visFrLeft || visFrRight) || (visFrBottom || visFrTop);
     }
 }
