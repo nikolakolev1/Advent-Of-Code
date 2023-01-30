36a9431f82bf1cef5b62be740d7d0a5656c4908b
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-01-30 15:47:40 +0000

    Day16 - Part 2 (Runs Slowly, But Still...)

--- 2022/Day16/src/Main.java
+++ 2022/Day16/src/Main.java
@@ -5,24 +5,35 @@ import java.util.*;
 public class Main {
     public static HashMap<String, Integer> valvesRates = new HashMap<>();
     public static ArrayList<Valve> allValves = new ArrayList<>();
-    public static int maxFlowRate = 0;
     private static int mostPressuredReleased = 0;
 
     public static void main(String[] args) {
         part1();
+        mostPressuredReleased = 0;
+        System.out.println();
+        part2();
     }
 
     private static void part1() {
-        loadDate("input.txt");
+        loadData("input.txt");
         addNeighbours();
         allValves.get(0).setAllDistanceFromValve();
-        addDistancesToValves();
-        findBestSolution(30, allValves.get(21), 0, createFirstAllValvesReleased());
+        setDistances();
+        findBestSolution(30, getValve("AA"), 0, createReleasedValves(), 0);
 
-        System.out.println("Part1 - Most pressured released: " + mostPressuredReleased);
+        System.out.println("=== Part 1 ===\nMost pressured released: " + mostPressuredReleased);
     }
 
-    private static void loadDate(String file) {
+    // Takes around 50 seconds to execute and works only when there are more valves that a single person can open
+    // Memoization should help, but it is challenging to implement
+    private static void part2() {
+        findBestSolution(26, getValve("AA"), 0, createReleasedValves(), 1);
+
+        System.out.println("=== Part 2 ===\nMost pressured released: " + mostPressuredReleased);
+    }
+
+    // loads the input
+    private static void loadData(String file) {
         try {
             File input = new File(file);
             Scanner myScanner = new Scanner(input);
@@ -37,8 +48,6 @@ public class Main {
                 Valve thisValve = new Valve(thisValveInfo[0], Integer.parseInt(thisValveInfo[1]), thisValveNeighbours);
                 valvesRates.put(thisValve.label, thisValve.flow_rate);
                 allValves.add(thisValve);
-
-                maxFlowRate += thisValve.flow_rate;
             }
 
             myScanner.close();
@@ -47,6 +56,14 @@ public class Main {
         }
     }
 
+    private static Valve getValve(String valveLabel) {
+        for (Valve valve : allValves) {
+            if (valve.label.equals(valveLabel)) return valve;
+        }
+        return allValves.get(0); // this shouldn't be reached
+    }
+
+    // sets each node's neighbouring nodes (each valve's neighbouring valves)
     private static void addNeighbours() {
         for (Valve currentValve : allValves) {
             for (String neighbourLabel : currentValve.neighboursLabels) {
@@ -60,29 +77,31 @@ public class Main {
         }
     }
 
-    private static void addDistancesToValves() {
+    // sets the distances between the nodes in the graph (the valves in the maze)
+    private static void setDistances() {
         for (Valve currentValve : allValves) {
             currentValve.setAllDistanceFromValve();
         }
     }
 
-    private static boolean[] createFirstAllValvesReleased() {
-        int allValvesSize = allValves.size();
-        boolean[] allValvesReleased = new boolean[allValvesSize];
+    // create the initial instance of releasedValves[] and set all the 0 flowing valves to 'opened'
+    private static boolean[] createReleasedValves() {
+        boolean[] releasedValves = new boolean[allValves.size()];
 
-        for (int i = 0; i < allValvesSize; i++) {
-            if (allValves.get(i).flow_rate == 0 || i == 0) allValvesReleased[i] = true;
+        for (int i = 0; i < allValves.size(); i++) {
+            if (allValves.get(i).flow_rate == 0) releasedValves[i] = true;
         }
 
-        return allValvesReleased;
+        return releasedValves;
     }
 
-    private static void findBestSolution(int remainingMinutes, Valve currentValve, int releasedPressure, boolean[] allValvesReleased) {
+    // dfs for the best solution
+    private static void findBestSolution(int remainingTime, Valve currentValve, int releasedPressure, boolean[] releasedValves, int helpers) {
         int allValvesSize = allValves.size();
 
         // check if all valves are opened
         boolean allReleased = true;
-        for (Boolean b : allValvesReleased) {
+        for (Boolean b : releasedValves) {
             if (!b) {
                 allReleased = false;
                 break;
@@ -97,19 +116,23 @@ public class Main {
                 Valve evaluatedValve = allValves.get(i);
 
                 // if the next valve is not opened yet, recurse down the path of opening it
-                if (!allValvesReleased[i]) {
-                    int newRemainingMinutes = remainingMinutes - currentValve.distanceFromValve.get(evaluatedValve.label) - 1;
+                if (!releasedValves[i]) {
+                    int newRemainingTime = remainingTime - currentValve.distanceToValve.get(evaluatedValve.label) - 1;
 
                     // if time's up - check if a new best solution was found
-                    if (newRemainingMinutes < 0) {
-                        if (releasedPressure > mostPressuredReleased) mostPressuredReleased = releasedPressure;
-                    } else if (evaluatedValve.flow_rate > 0) {
-                        boolean[] newAllValvesReleased = new boolean[allValvesSize];
-                        System.arraycopy(allValvesReleased, 0, newAllValvesReleased, 0, allValvesSize);
-                        newAllValvesReleased[i] = true;
-                        int newReleasedPressure = releasedPressure + (evaluatedValve.flow_rate * newRemainingMinutes);
+                    if (newRemainingTime < 0) {
+                        if (--helpers < 0) {
+                            if (releasedPressure > mostPressuredReleased) mostPressuredReleased = releasedPressure;
+                        } else {
+                            findBestSolution(26, getValve("AA"), releasedPressure, releasedValves, helpers);
+                        }
+                    } else { // if (evaluatedValve.flow_rate > 0) {
+                        boolean[] newReleasedValves = new boolean[allValvesSize];
+                        System.arraycopy(releasedValves, 0, newReleasedValves, 0, allValvesSize);
+                        newReleasedValves[i] = true;
+                        int newReleasedPressure = releasedPressure + (evaluatedValve.flow_rate * newRemainingTime);
 
-                        findBestSolution(newRemainingMinutes, evaluatedValve, newReleasedPressure, newAllValvesReleased);
+                        findBestSolution(newRemainingTime, evaluatedValve, newReleasedPressure, newReleasedValves, helpers);
                     }
                 }
             }
@@ -121,7 +144,7 @@ public class Main {
         public int flow_rate;
         public String[] neighboursLabels;
         public ArrayList<Valve> neighbours = new ArrayList<>();
-        public HashMap<String, Integer> distanceFromValve = new HashMap<>();
+        public HashMap<String, Integer> distanceToValve = new HashMap<>();
 
         public Valve(String label, int flow_rate, String[] neighbours) {
             this.label = label;
@@ -135,15 +158,15 @@ public class Main {
             int numNeighbours = 1;
             valvesQueue.add(this);
 
-            while (distanceFromValve.size() < allValves.size()) {
+            while (distanceToValve.size() < allValves.size()) {
                 int neighboursNextRound = 0;
 
                 for (int j = 0; j < numNeighbours; j++) {
                     Valve assessedValve = valvesQueue.poll();
 
                     assert assessedValve != null;
-                    if (!distanceFromValve.containsKey(assessedValve.label)) {
-                        distanceFromValve.put(assessedValve.label, distanceCounter);
+                    if (!distanceToValve.containsKey(assessedValve.label)) {
+                        distanceToValve.put(assessedValve.label, distanceCounter);
                     }
 
                     for (Valve assessedValveNeighbour : assessedValve.neighbours) {
