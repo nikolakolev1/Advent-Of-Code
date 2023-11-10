bea9fb9fca3cd3fccc956590ba8f96e34fce9263
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-11-10 11:12:09 +0000

    Adding boilerplate code

--- 2023/Day23/src/Main.java
+++ 2023/Day23/src/Main.java
@@ -1,5 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        loadData("data.txt");
+        part1();
+        part2();
+    }
+
+    private static void loadData(String filename) {
+        try {
+            File input = new File(filename);
+            Scanner scanner = new Scanner(input);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+            }
+
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error loading data: " + e.getMessage());
+        }
+    }
+
+    private static void part1() {
+        Helper.printAnswer(1, -1);
+    }
+
+    private static void part2() {
+        Helper.printAnswer(2, -1);
     }
 }
