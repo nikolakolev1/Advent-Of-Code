99053cb949e6e1eaf50f8bae0a4b39bab7e6adcf
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-01 14:06:49 +0000

    Added comments to Day1

--- y-2023/src/Day1/Day1.java
+++ y-2023/src/Day1/Day1.java
@@ -69,7 +69,14 @@ public class Day1 {
         Helper.printAnswer(2, sum);
     }
 
+    /**
+     * Returns the first numeric digit in the string and its index
+     *
+     * @param str = String to search
+     * @return = int[] {digit, index}
+     */
     private static int[] getFirstNumericDigit(String str) {
+        // Loop through the string and return the first numeric character
         for (int i = 0; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
                 return new int[]{Character.getNumericValue(str.charAt(i)), i};
@@ -79,7 +86,14 @@ public class Day1 {
         throw new IllegalArgumentException("No digit found in string");
     }
 
+    /**
+     * Returns the last numeric digit in the string and its index
+     *
+     * @param str = String to search
+     * @return = int[] {digit, index}
+     */
     private static int[] getLastNumericDigit(String str) {
+        // Loop through the string backwards and return the first numeric character
         for (int i = str.length() - 1; i >= 0; i--) {
             if (Character.isDigit(str.charAt(i))) {
                 return new int[]{Character.getNumericValue(str.charAt(i)), i};
@@ -89,7 +103,14 @@ public class Day1 {
         throw new IllegalArgumentException("No digit found in string");
     }
 
+    /**
+     * Returns true if the string contains a spelled digit
+     *
+     * @param str = String to search
+     * @return = boolean of whether the string contains a spelled digit
+     */
     private static boolean hasSpelledDigit(String str) {
+        // For each spelled digit, check if the string contains it
         for (String spelledDigit : SPELLED_DIGITS) {
             if (str.contains(spelledDigit)) {
                 return true;
@@ -99,17 +120,24 @@ public class Day1 {
         return false;
     }
 
+    /**
+     * Returns the first spelled digit in the string and its index
+     *
+     * @param str = String to search
+     * @return = int[] {digit, index}
+     */
     private static int[] getFirstSpelledDigit(String str) {
         for (int i = 0; i < str.length() - 2; i++) {
+            // Skip if the character is a digit (a spelled digit cannot contain a numeric)
             if (Character.isDigit(str.charAt(i))) {
                 continue;
             }
 
+            // For each spelled digit, check if the string starts with it
             for (int j = 0; j < SPELLED_DIGITS.length; j++) {
-                if (i + SPELLED_DIGITS[j].length() <= str.length()) {
-                    if (str.startsWith(SPELLED_DIGITS[j], i)) {
-                        return new int[]{j + 1, i};
-                    }
+                // Make sure the spelled digit doesn't go out of bounds
+                if (i + SPELLED_DIGITS[j].length() <= str.length() && str.startsWith(SPELLED_DIGITS[j], i)) {
+                    return new int[]{j + 1, i};
                 }
             }
         }
@@ -117,20 +145,30 @@ public class Day1 {
         throw new IllegalArgumentException("No spelled digit found in string");
     }
 
+    /**
+     * Returns the last spelled digit in the string and its index
+     *
+     * @param str = String to search
+     * @return = int[] {digit, index}
+     */
     private static int[] getLastSpelledDigit(String str) {
         for (int i = str.length() - 3; i >= 0; i--) {
+            // Skip if the character is a digit (a spelled digit cannot contain a numeric)
             if (Character.isDigit(str.charAt(i))) {
                 i -= 2;
-            } else {
-                int end = str.length();
-                if (i + 5 < str.length()) {
-                    end = i + 5;
-                }
+                continue;
+            }
 
-                for (int j = 0; j < SPELLED_DIGITS.length; j++) {
-                    if (str.substring(i, end).contains(SPELLED_DIGITS[j])) {
-                        return new int[]{j + 1, i};
-                    }
+            // Make sure the spelled digit doesn't go out of bounds
+            int end = str.length();
+            if (i + 5 < str.length()) {
+                end = i + 5;
+            }
+
+            // For each spelled digit, check if the string has it
+            for (int j = 0; j < SPELLED_DIGITS.length; j++) {
+                if (str.substring(i, end).contains(SPELLED_DIGITS[j])) {
+                    return new int[]{j + 1, i};
                 }
             }
         }
@@ -138,6 +176,12 @@ public class Day1 {
         throw new IllegalArgumentException("No spelled digit found in string");
     }
 
+    /**
+     * Extracts the number from the string, without checking for spelled digits
+     *
+     * @param str = String to extract number from
+     * @return = the extracted number as an int
+     */
     private static int extractNumber_NumericOnly(String str) {
         int[] firstDigit = getFirstNumericDigit(str);
         int[] lastDigit = getLastNumericDigit(str);
@@ -146,9 +190,16 @@ public class Day1 {
         return buildInteger(digits);
     }
 
+    /**
+     * Builds an integer from an array of digits
+     *
+     * @param digits = array of digits
+     * @return = the integer
+     */
     private static int buildInteger(int[] digits) {
         int num = 0;
 
+        // For each digit, move one place to the left and add the digit
         for (int digit : digits) {
             num *= 10;
             num += digit;
