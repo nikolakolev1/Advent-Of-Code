9e503d387f62c39ad6f088bd14e4b328175031d3
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-02 11:32:50 +0000

    Day 2 - Both Parts ✅

--- y-2023/src/Day2/Day2.java
+++ y-2023/src/Day2/Day2.java
@@ -3,9 +3,14 @@ package Day2;
 import Helpers.Helper;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day2 {
+    private static final ArrayList<int[]> data = new ArrayList<>();
+    private static final int RED_I = 0, GREEN_I = 1, BLUE_I = 2;
+    private static final int RED_MAX = 12, GREEN_MAX = 13, BLUE_MAX = 14;
+
     public static void main(String[] args) {
         loadData();
 
@@ -29,7 +34,25 @@ public class Day2 {
             Scanner scanner = new Scanner(input);
 
             while (scanner.hasNextLine()) {
+                int[] rgbValues = new int[3];
+
                 String line = scanner.nextLine();
+                String[] split = line.split(" ");
+
+                // store the max value for each color
+                for (int i = 3; i < split.length; i += 2) {
+                    if (split[i].startsWith("red")) {
+                        rgbValues[RED_I] = Math.max(Integer.parseInt(split[i - 1]), rgbValues[RED_I]);
+                    } else if (split[i].startsWith("green")) {
+                        rgbValues[GREEN_I] = Math.max(Integer.parseInt(split[i - 1]), rgbValues[GREEN_I]);
+                    } else if (split[i].startsWith("blue")) {
+                        rgbValues[BLUE_I] = Math.max(Integer.parseInt(split[i - 1]), rgbValues[BLUE_I]);
+                    } else {
+                        throw new Exception("Invalid color: " + split[i]);
+                    }
+                }
+
+                data.add(rgbValues);
             }
 
             scanner.close();
@@ -39,10 +62,28 @@ public class Day2 {
     }
 
     private static int part1() {
-        return -1;
+        int dataSize = data.size(); // avoid calling .size() every iteration
+
+        int sum = 0;
+
+        // for each game, check if the max value for each color > the max possible value
+        for (int i = 0; i < dataSize; i++) {
+            if (data.get(i)[RED_I] <= RED_MAX && data.get(i)[GREEN_I] <= GREEN_MAX && data.get(i)[BLUE_I] <= BLUE_MAX) {
+                sum += (i + 1);
+            }
+        }
+
+        return sum;
     }
 
     private static int part2() {
-        return -1;
+        int sum = 0;
+
+        // for each game, add the product of the max values for each color
+        for (int[] rgbValues : data) {
+            sum += (rgbValues[RED_I] * rgbValues[GREEN_I] * rgbValues[BLUE_I]);
+        }
+
+        return sum;
     }
 }
