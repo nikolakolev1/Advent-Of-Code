2c748cf6c6d1a1a73d8d00471219c71e05e5773f
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-04 17:34:01 +0000

    Day 4 - Both Parts ✅

--- y-2023/src/Day4/Day4.java
+++ y-2023/src/Day4/Day4.java
@@ -1,12 +1,20 @@
 package Day4;
 
-import Helpers.Helper;
 import Helpers.Day;
+import Helpers.Helper;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day4 implements Day {
+    private final List<HashSet<Integer>> winningNums = new ArrayList<>();
+    private final List<ArrayList<Integer>> yourNums = new ArrayList<>();
+    private final List<Integer> copies = new ArrayList<>();
+    private static final int WINNING = 0, YOUR = 1;
+
     public static void main(String[] args) {
         Day day4 = new Day4();
         day4.loadData(Helper.filename(4));
@@ -22,6 +30,23 @@ public class Day4 implements Day {
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
+                String[] nums = line.split("\\|");
+
+                String[] winning = nums[WINNING].split(":")[1].split(" ");
+                HashSet<Integer> winningSet = new HashSet<>();
+                for (String num : winning) {
+                    if (!num.isBlank()) winningSet.add(Integer.parseInt(num.trim()));
+                }
+
+                String[] your = nums[YOUR].split(" ");
+                ArrayList<Integer> yourList = new ArrayList<>();
+                for (String num : your) {
+                    if (!num.isBlank()) yourList.add(Integer.parseInt(num.trim()));
+                }
+
+                winningNums.add(winningSet);
+                yourNums.add(yourList);
+                copies.add(1); // for part 2
             }
 
             scanner.close();
@@ -32,11 +57,58 @@ public class Day4 implements Day {
 
     @Override
     public int part1() {
-        return -1;
+        int sum = 0;
+
+        int scratchcards = winningNums.size(); // to avoid calling size() every iteration
+
+        for (int i = 0; i < scratchcards; i++) {
+            HashSet<Integer> winningSet = winningNums.get(i);
+            ArrayList<Integer> yourList = yourNums.get(i);
+
+            int matches = 0;
+
+            for (int num : yourList) {
+                if (winningSet.contains(num)) {
+                    matches++;
+                }
+            }
+
+            if (matches != 0) {
+                sum += (int) Math.pow(2, matches - 1);
+            }
+        }
+
+        return sum;
     }
 
     @Override
     public int part2() {
-        return -1;
+        int scratchcards = winningNums.size(); // to avoid calling size() every iteration
+
+        for (int i = 0; i < scratchcards; i++) {
+            HashSet<Integer> winningSet = winningNums.get(i);
+            ArrayList<Integer> yourList = yourNums.get(i);
+
+            int matches = 0;
+
+            for (int num : yourList) {
+                if (winningSet.contains(num)) {
+                    matches++;
+                }
+            }
+
+            // to avoid IndexOutOfBoundsException
+            if (i + matches >= copies.size()) {
+                matches = copies.size() - i;
+            }
+
+            for (int j = 0; j < matches; j++) {
+                copies.set(i + j + 1, (copies.get(i + j + 1) + copies.get(i)));
+            }
+        }
+
+        // return the sum of all copies
+        return copies.stream()
+                .reduce(0, Integer::sum);
     }
 }
