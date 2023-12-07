0ab22f47829bf09aa981091319ba50b8b065658f
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2023-12-07 15:31:04 +0100

    Update Main.java

--- y-2023/src/Main.java
+++ y-2023/src/Main.java
@@ -1,13 +1,19 @@
 import General.Helper;
 
 public class Main {
+    static String warning = "solveAndPrint_Time() is not the most accurate way to measure the time of a program, because some days do part of the computation during the loadData() method\n";
+
     public static void main(String[] args) {
+        System.out.println(warning);
+
         try {
             Helper.solveAndPrint_Time(1);
             Helper.solveAndPrint_Time(2);
             Helper.solveAndPrint_Time(3);
             Helper.solveAndPrint_Time(4);
             Helper.solveAndPrint_Time(5);
+            Helper.solveAndPrint_Time(6);
+            Helper.solveAndPrint_Time(7);
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
