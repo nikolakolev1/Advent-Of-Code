59f97348a788b361d9e6badadcccd6868087aad2
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2024-12-03 10:07:26 +0100

    Day 3 - Wrote boilerplate code

--- y-2024/src/Days/Day3/Day3.java
+++ y-2024/src/Days/Day3/Day3.java
@@ -1,4 +1,41 @@
 package Days.Day3;
 
-public class Day3 {
+import General.Day;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day3 implements Day {
+    private List<String> input = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Day day3 = new Day3();
+        day3.loadData("data/day3/input.txt");
+        System.out.println(day3.part1());
+        System.out.println(day3.part2());
+    }
+
+    @Override
+    public void loadData(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                input.add(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading data: " + e.getMessage());
+
+        }
+    }
+
+    @Override
+    public String part1() {
+        return null;
+    }
+
+    @Override
+    public String part2() {
+        return null;
+    }
 }
