0b1d5c472072fe455e9ca3acf75509326589255f
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2024-12-07 12:02:55 +0100

    Day 6 - Both Parts ✅

--- y-2024/src/Days/Day6/Day6.java
+++ y-2024/src/Days/Day6/Day6.java
@@ -6,9 +6,22 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Day6 implements Day {
+    private boolean[][] map; // false = open, true = wall
+    private Coordinates guard;
+    private Coordinates guardStart;
+    private boolean guardLeft = false;
+    private boolean[][] visitedP1;
+
+    private record Coordinates(int x, int y, Direction direction) {
+    }
+
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
     public static void main(String[] args) {
         Day day6 = new Day6();
-        day6.loadData("data/day6/input_test.txt");
+        day6.loadData("data/day6/input.txt");
         System.out.println(day6.part1());
         System.out.println(day6.part2());
     }
@@ -16,8 +29,31 @@ public class Day6 implements Day {
     @Override
     public void loadData(String filename) {
         try (Scanner scanner = new Scanner(new File(filename))) {
+
+            int x = 0;
+            int y = 0;
+
             while (scanner.hasNextLine()) {
-                // TODO: Load data
+                String[] line = scanner.nextLine().split("");
+
+                // initialize the map
+                if (map == null) {
+                    map = new boolean[line.length][line.length];
+                }
+
+                for (String tile : line) {
+                    switch (tile) {
+                        case "#" -> map[x][y] = true;
+                        case "^" -> {
+                            guardStart = new Coordinates(x, y, Direction.UP);
+                            guard = new Coordinates(x, y, Direction.UP);
+                        }
+                    }
+                    x++;
+                }
+
+                x = 0;
+                y++;
             }
         } catch (Exception e) {
             System.out.println("Error loading data: " + e.getMessage());
@@ -26,19 +62,162 @@ public class Day6 implements Day {
 
     @Override
     public String part1() {
-        int sum = 0;
+        boolean[][] visited = new boolean[map.length][map.length]; // false = not visited, true = visited
 
-        // TODO: Implement part 1
+        while (!guardLeft) {
+            visited[guard.x][guard.y] = true;
+            move();
+        }
 
-        return String.valueOf(sum);
+        visitedP1 = visited;
+
+        return String.valueOf(countVisited(visited));
     }
 
     @Override
     public String part2() {
         int sum = 0;
 
-        // TODO: Implement part 2
+        for (int y = 0; y < visitedP1.length; y++) {
+            for (int x = 0; x < visitedP1.length; x++) {
+                if (visitedP1[x][y]) {
+                    // reset guard
+                    guard = new Coordinates(guardStart.x(), guardStart.y(), guardStart.direction());
+                    guardLeft = false;
+
+                    if (guardStart.x() == x && guardStart.y() == y) {
+                        continue;
+                    }
+
+                    map[x][y] = true;
+
+                    for (int i = 0; i < 10000; i++) { // hardcoded number of steps
+                        if (!guardLeft) {
+                            move();
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (!guardLeft) sum++;
+                    else guardLeft = false;
+
+                    map[x][y] = false;
+                }
+            }
+        }
 
         return String.valueOf(sum);
+    }
+
+    private void move() {
+        if (isBlocked()) {
+            turnRight();
+        } else {
+            if (!guardLeft) moveForward();
+        }
+    }
+
+    private boolean isBlocked() {
+        switch (guard.direction) {
+            case UP -> {
+                if (guard.y - 1 < 0) {
+                    guardLeft = true;
+                    return false;
+                }
+                return map[guard.x][guard.y - 1];
+            }
+            case DOWN -> {
+                if (guard.y + 1 >= map.length) {
+                    guardLeft = true;
+                    return false;
+                }
+                return map[guard.x][guard.y + 1];
+            }
+            case LEFT -> {
+                if (guard.x - 1 < 0) {
+                    guardLeft = true;
+                    return false;
+                }
+                return map[guard.x - 1][guard.y];
+            }
+            case RIGHT -> {
+                if (guard.x + 1 >= map.length) {
+                    guardLeft = true;
+                    return false;
+                }
+                return map[guard.x + 1][guard.y];
+            }
+        }
+
+        System.out.println("Should never reach this point");
+        return false;
+    }
+
+    private void moveForward() {
+        switch (guard.direction) {
+            case UP -> guard = new Coordinates(guard.x, guard.y - 1, guard.direction);
+            case DOWN -> guard = new Coordinates(guard.x, guard.y + 1, guard.direction);
+            case LEFT -> guard = new Coordinates(guard.x - 1, guard.y, guard.direction);
+            case RIGHT -> guard = new Coordinates(guard.x + 1, guard.y, guard.direction);
+        }
+    }
+
+    private void turnRight() {
+        switch (guard.direction) {
+            case UP -> guard = new Coordinates(guard.x, guard.y, Direction.RIGHT);
+            case DOWN -> guard = new Coordinates(guard.x, guard.y, Direction.LEFT);
+            case LEFT -> guard = new Coordinates(guard.x, guard.y, Direction.UP);
+            case RIGHT -> guard = new Coordinates(guard.x, guard.y, Direction.DOWN);
+        }
+    }
+
+    private int countVisited(boolean[][] visited) {
+        int count = 0;
+        for (boolean[] row : visited) {
+            for (boolean tile : row) {
+                if (tile) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void printMap() {
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map.length; x++) {
+                if (guard.x == x && guard.y == y) {
+                    System.out.print("^");
+                } else if (map[x][y]) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private void printVisited(boolean[][] visited) {
+        for (int y = 0; y < visited.length; y++) {
+            for (int x = 0; x < visited.length; x++) {
+                if (guard.x == x && guard.y == y) {
+                    switch (guard.direction) {
+                        case UP -> System.out.print("^");
+                        case DOWN -> System.out.print("v");
+                        case LEFT -> System.out.print("<");
+                        case RIGHT -> System.out.print(">");
+                    }
+                } else if (visited[x][y]) {
+                    System.out.print("X");
+                } else if (map[x][y]) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
     }
 }
