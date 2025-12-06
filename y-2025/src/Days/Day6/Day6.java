6311c3d8c2cb060ad2f1298dd4054724e6a2342c
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2025-12-06 18:36:28 +0200

    Day6 - Boilerplate

---
+++ y-2025/src/Days/Day6/Day6.java
@@ -0,0 +1,44 @@
+package Days.Day6;
+
+import General.Day;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Day6 implements Day {
+    public static void main(String[] args) {
+        Day day6 = new Day6();
+        day6.loadData("data/day6/input_test.txt");
+        System.out.println(day6.part1());
+        System.out.println(day6.part2());
+    }
+
+    @Override
+    public void loadData(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            while (scanner.hasNextLine()) {
+                // TODO: Load data
+            }
+        } catch (Exception e) {
+            System.out.println("Error loading data: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String part1() {
+        int sum = 0;
+
+        // TODO: Implement part 1
+
+        return String.valueOf(sum);
+    }
+
+    @Override
+    public String part2() {
+        int sum = 0;
+
+        // TODO: Implement part 2
+
+        return String.valueOf(sum);
+    }
+}
